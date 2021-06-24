c66658844cb263f365a0cd6f77b0e456e483e488
Pbone3 <pbone785@gmail.com>
2021-06-23 22:35:01 -0500

    Homepage and corporeal nerf

--- Prefixes/Content/Corporeal.cs
+++ Prefixes/Content/Corporeal.cs
@@ -6,7 +6,7 @@
         public override float Value => 1f - 0.4607f;
 
         public override float DamageMult => 0.9f;
-        public override float KnockbackMult => 1.15f;
+        public override float KnockbackMult => 0.85f;
         public override float UseTimeMult => 1f;
         public override float ManaMult => 1.2f;
 
